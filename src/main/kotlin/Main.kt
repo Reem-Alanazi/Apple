3c265b985e947a4e02fc29757e5d1ef5c0511f02
reem <Reem.Fawaz.ruh.k@tuwaiq.edu.sa>
2021-10-12 15:02:37 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+const val apple = 50
+const val Dozens2 = 12
+fun main(args: Array<String>) {
+
+    var Dozens = 12
+    var noOfDozens  = apple / Dozens
+    var remainApple = apple % Dozens
+    println("Number Of Dezone ? $noOfDozens")
+    println("Remain Number of Apple ? $remainApple")
+
+    // Another way with Teatcher
+    var noOfDozens1  = apple / 12
+    var remainApple1 = apple % noOfDozens1
+    println("Number Of Dezone ? $noOfDozens1")
+    println("Remain Number of Apple ? $remainApple1")
+
+    // Another way with Teatcher 2
+    var noOfDozens2  = apple / Dozens2
+    var remainApple2 = apple % noOfDozens2
+    println("Number Of Dezone ? $noOfDozens2")
+    println("Remain Number of Apple ? $remainApple2")
+}
